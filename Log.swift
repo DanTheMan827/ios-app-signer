c8b0c9856ebb265abda1c976172368f9e0dfc9a5
terry1994 <terry1994dev@gmail.com>
2016-12-12 19:56:42 +0100

    *Updated to Swift 3.

--- Log.swift
+++ Log.swift
@@ -8,23 +8,23 @@
 
 import Foundation
 class Log {
-    static let mainBundle = NSBundle.mainBundle()
+    static let mainBundle = Bundle.main
     static let bundleID = mainBundle.bundleIdentifier
     static let bundleName = mainBundle.infoDictionary!["CFBundleName"]
     static let bundleVersion = mainBundle.infoDictionary!["CFBundleShortVersionString"]
     static let tempDirectory = NSTemporaryDirectory()
-    static var logName = Log.tempDirectory.stringByAppendingPathComponent("\(Log.bundleID!)-\(NSDate().timeIntervalSince1970).log")
+    static var logName = Log.tempDirectory.stringByAppendingPathComponent("\(Log.bundleID!)-\(Date().timeIntervalSince1970).log")
     
-    static func write(value:String) {
-        let formatter = NSDateFormatter()
+    static func write(_ value:String) {
+        let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         
         
-        if let outputStream = NSOutputStream(toFileAtPath: logName, append: true) {
+        if let outputStream = OutputStream(toFileAtPath: logName, append: true) {
             outputStream.open()
-            let text = "\(formatter.stringFromDate(NSDate())) \(value)\n"
-            let data = text.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
-            outputStream.write(UnsafePointer(data.bytes), maxLength: data.length)
+            let text = "\(formatter.string(from: Date())) \(value)\n"
+            let data = text.data(using: String.Encoding.utf8, allowLossyConversion: false)!
+            outputStream.write((data as NSData).bytes.bindMemory(to: UInt8.self, capacity: data.count), maxLength: data.count)
             outputStream.close()
         }
         NSLog(value)
